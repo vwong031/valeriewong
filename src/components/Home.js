103094d645f3fd107ad4a75bd256a8543cc11709
Val <vwong031@ucr.edu>
2024-05-21 21:39:17 -0700

    typing animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,40 @@
+import React from 'react';
+import { ReactTyped } from 'react-typed';
 import './Home.css';
 
 function Home() {
+  // const [typedText, setTypedText] = useState('');
+  // const fullText = "Hi, I'm Valerie Wong!";
+  // const typingSpeed = 100;
+  // const resetDelay = 2000;
+
+  // useEffect(() => {
+  //   let i = 0;
+  //   let isMounted = true;
+    
+  //   const typeText = () => {
+  //     if (isMounted) {
+  //       if (i < fullText.length) {
+  //         setTypedText((prev) => prev + fullText[i]);
+  //         ++i;
+  //       }
+  //       else {
+  //         setTimeout(() => {
+  //           setTypedText('');
+  //           i = 0;
+  //         }, resetDelay);
+  //       }
+  //     }
+  //   };
+
+  //   let interval = setInterval(typeText, typingSpeed);
+
+  //   return () => {
+  //     isMounted = false;
+  //     clearInterval(interval);
+  //   };
+  // }, []);
+
   return (
     <div id="Home">
       <div className="container">
@@ -11,7 +45,16 @@ function Home() {
         />
         <div className="name-and-description barlow-regular">
           <div className="name">
-            Hi, I'm Valerie Wong!
+            {/* Hi, I'm Valerie Wong! */}
+            {/* {typedText} <span className="cursor">|</span> */}
+            <ReactTyped 
+              strings={[
+                "Hi, I'm Valerie Wong!",
+              ]}
+              typeSpeed={170}
+              backSpeed={130}
+              loop
+            />
           </div>
           <div className="description">
             I am a 4th year computer science student at the University of California, Riverside.

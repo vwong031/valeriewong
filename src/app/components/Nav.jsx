9b76dc009894f5fecf3508715cfc03d1861ccf31
Val <vwong031@ucr.edu>
2025-12-30 00:43:16 -0800

    feat: Add initial landing page setup/structure

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -2,7 +2,7 @@ import ColorButton from "./ColorButton";
 
 export default function Nav() {
   return (
-    <div className="flex items-center justify-between px-8 py-6 text-2xl text-white">
+    <div className="flex items-center justify-between px-8 py-8 text-2xl text-white">
       <div className="bg-palmLeaf">VKW</div>
       <div className="bg-antiqueWhite border-antiqueWhite mx-auto w-1/3 rounded-3xl border border-2 text-black">
         <button className="w-1/5">Home</button>
--- src/app/landing/page.js
+++ src/app/landing/page.js
@@ -1,9 +1,31 @@
+import ColorButton from "../components/ColorButton";
+import BorderButton from "../components/BorderButton";
 import Nav from "../components/Nav";
+import Image from "next/image";
 
 export default function Landing() {
   return (
     <div className="bg-floralWhite">
       <Nav />
+      <div className="mx-auto flex w-11/12 justify-center pt-8 pb-10">
+        <Image
+          src="/profile-pic.jpeg"
+          alt="Picture of me wearing a black leather jacket, a gray shirt, and black pants sitting in front of the VISA sign."
+          width={325}
+          height={325}
+          className="mx-10 rounded-3xl"
+        />
+        <div className="mx-10">
+          <h2 className="text-7xl font-bold">
+            Hi, I'm Valerie Wong (animation)
+          </h2>
+          <p className="text-2xl">Short Description</p>
+          <div className="flex">
+            <ColorButton text="Resume"></ColorButton>
+            <BorderButton text="View More"></BorderButton>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

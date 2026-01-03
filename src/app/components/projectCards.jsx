83ba674cb46f33fb5eddd0092f4cd8700cb1094a
Val <vwong031@ucr.edu>
2026-01-02 21:22:48 -0800

    style: created example grid cards for project section

--- src/app/components/projectCards.jsx
+++ src/app/components/projectCards.jsx
@@ -2,13 +2,41 @@ import Image from "next/image";
 
 export default function projectCards({ projects }) {
   return (
-    <div className="w-9/12 mx-auto mt-20">
-      <Image 
+    <div className="mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 lg:grid-cols-3 mt-20 flex w-9/12">
+      {projects.map((project, index) => (
+        <div
+          key={index}
+          className="flex flex-col items-center justify-center rounded-2xl border border-black/5 bg-antiqueWhite p-8"
+        >
+          <div className="">
+            <Image
+              src={project.image}
+              alt="placeholder image"
+              className="rounded-xl"
+              width={300}
+              height={300}
+            />
+            <h2 className="mt-3 text-3xl font-bold">{project.title}</h2>
+            <p className="text-xl text-gray-600">{project.description}</p>
+            <div className="mt-2 flex">
+              {project.skills.map((skill, skillIndex) => (
+                <span
+                  key={skillIndex}
+                  className="mr-2 rounded border border-black/5 bg-white px-2 py-1 text-sm font-bold uppercase tracking-wider text-gray-700"
+                >
+                  {skill}
+                </span>
+              ))}
+            </div>
+          </div>
+        </div>
+      ))}
+      {/* <Image 
         src={projects[0].image}
         alt="example image"
         width={300}
         height={300}
-      />
+      /> */}
     </div>
   );
 }

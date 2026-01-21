a7bca65e403708da497f8985e202e4d76b0b1cde
Val <vwong031@ucr.edu>
2026-01-20 22:19:00 -0800

    style: add burger menu item design

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -1,14 +1,20 @@
+"use client";
+
+import { useState } from "react";
 import ColorButton from "./ColorButton";
 import Link from "next/link";
 import { GiTurtle } from "react-icons/gi";
+import { IoMenu } from "react-icons/io5";
 
 export default function Nav() {
   return (
     <div className="sticky top-0 z-50 flex w-full items-center justify-between border border-black/5 bg-floralWhite px-8 py-4 text-2xl text-white shadow-sm">
-      <div className="flex flex-col justify-center items-center">
-        <Link href="#landing"><GiTurtle size={35} className="text-palmLeaf" /></Link>
+      <div className="flex flex-col items-center justify-center">
+        <Link href="#landing">
+          <GiTurtle size={35} className="text-palmLeaf" />
+        </Link>
       </div>
-      <div className="mx-auto w-1/3 rounded-3xl border border-2 border-antiqueWhite border-black/5 bg-antiqueWhite text-black">
+      <div className="mx-auto hidden w-1/3 rounded-3xl border border-2 border-antiqueWhite border-black/5 bg-antiqueWhite text-black md:block">
         <Link href="#landing">
           <button className="w-1/5">Home</button>
         </Link>
@@ -25,9 +31,12 @@ export default function Nav() {
           <button className="w-1/5">Fun</button>
         </Link>
       </div>
-      <div>
+      <div className="hidden md:block">
         <ColorButton href="#contact" text="Contact Me"></ColorButton>
       </div>
+      <button className="block text-black md:hidden">
+          <IoMenu className="text-palmLeaf" size={30} />
+      </button>
     </div>
   );
 }
